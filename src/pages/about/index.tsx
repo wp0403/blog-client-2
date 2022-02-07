385431ab800133dce90e29c71b1be77433b416d1
wengpeng0403 <webwp0403@163.com>
2022-02-07 10:05:11 +0800

    fix:

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -4,17 +4,25 @@
  * @Author: WangPeng
  * @Date: 2021-12-29 11:03:48
  * @LastEditors: WangPeng
- * @LastEditTime: 2022-01-05 17:05:22
+ * @LastEditTime: 2022-02-06 08:10:37
  */
 import React from 'react';
-import LoadingCard from '@/components/LoadingCard';
+import type { UserDate } from '@/utils/globalDataUtils';
+import { getGlobalUserData } from '@/utils/globalDataUtils';
+// import ReactMarkdown from 'react-markdown';
 import styles from './index.less';
 
 const About = () => {
+  const userData = getGlobalUserData() as UserDate;
+
+  console.log(userData);
+
   return (
-    <div>
-      about 关于
-      <LoadingCard />
+    <div className={styles.about}>
+      <div className={styles.content}>
+        {userData.about_page}
+        {/* <ReactMarkdown>{userData.about_page}</ReactMarkdown> */}
+      </div>
     </div>
   );
 };
