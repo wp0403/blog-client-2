c521d3d1e8f62bf2e994051d4acb035659c885a0
wengpeng0403 <webwp0403@163.com>
2022-02-16 10:36:28 +0800

    fix:

--- src/pages/timeAxis/index.tsx
+++ src/pages/timeAxis/index.tsx
@@ -4,13 +4,24 @@
  * @Author: WangPeng
  * @Date: 2021-12-29 11:07:43
  * @LastEditors: WangPeng
- * @LastEditTime: 2022-02-10 14:03:08
+ * @LastEditTime: 2022-02-15 18:33:40
  */
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSize } from 'ahooks';
+import { setBg } from '@/utils/utils';
 import styles from './index.less';
 
 const TimeAxis = () => {
-  return <div className={styles.timeAxis}>aaa</div>;
+  useEffect(() => {
+    setBg(false);
+  }, []);
+  return (
+    <div className={styles.timeAxis}>
+      <div className="header"></div>
+      <div className="main"></div>
+      <div className="footer"></div>
+    </div>
+  );
 };
 
 export default TimeAxis;
