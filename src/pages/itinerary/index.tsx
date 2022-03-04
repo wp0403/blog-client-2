a9fb1a760c4c0d64f1647d689a1de582c8e02e55
wengpeng0403 <webwp0403@163.com>
2022-03-04 18:03:26 +0800

    fix:

--- src/pages/itinerary/index.tsx
+++ src/pages/itinerary/index.tsx
@@ -4,13 +4,13 @@
  * @Author: WangPeng
  * @Date: 2021-12-29 11:13:12
  * @LastEditors: WangPeng
- * @LastEditTime: 2022-03-04 18:00:26
+ * @LastEditTime: 2022-03-04 18:03:18
  */
 import React from 'react';
 import styles from './index.less';
 
 const Itinerary = () => {
-  return <div>itinerary 旅行日记aaaa</div>;
+  return <div>itinerary 旅行日记aaa</div>;
 };
 
 export default Itinerary;
