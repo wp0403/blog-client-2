9661c7404f32f5fd96726e76124eaa7fbd3b546f
wengpeng0403 <webwp0403@163.com>
2022-03-17 19:18:58 +0800

    fix:

--- src/pages/itinerary-details/index.tsx
+++ src/pages/itinerary-details/index.tsx
@@ -4,23 +4,21 @@
  * @Author: WangPeng
  * @Date: 2022-03-10 18:03:32
  * @LastEditors: WangPeng
- * @LastEditTime: 2022-03-10 18:09:53
+ * @LastEditTime: 2022-03-17 14:42:40
  */
-import React from 'react';
+import React, { useEffect } from 'react';
 import SysIcon from '@/components/SysIcon';
+import { setBg, addLayoutNavStyle } from '@/utils/utils';
 import styles from './index.less';
 
 const ItineraryDetails = () => {
-  return (
-    <div className={styles.itineraryDetails}>
-      <div className={styles.header}>
-        <div className={styles.header_top}>这是一个大标题大大大标题</div>
-        <div className={styles.header_bottom}></div>
-      </div>
-      <div className={styles.main}></div>
-      <div className={styles.footer}></div>
-    </div>
-  );
+  // 初始化
+  useEffect(() => {
+    addLayoutNavStyle();
+    setBg(false);
+  }, []);
+
+  return <div className={styles.itineraryDetails}>div.</div>;
 };
 
 export default ItineraryDetails;
